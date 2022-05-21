742d7ae89d124532960a1f188dde3b3be466c51d
rafaelvieiracosta <rvc.vieira@outlook.com>
2022-05-21 16:23:34 -0300

    feat: slider custom controls

--- js/script.js
+++ js/script.js
@@ -3,4 +3,4 @@ import { Slider, SliderNav } from "./slider.js";
 const slider = new SliderNav(".slider", ".slider-wrapper");
 slider.init();
 slider.addArrow(".prev", ".next");
-slider.addControl();
+slider.addControl(".custom-controls");
